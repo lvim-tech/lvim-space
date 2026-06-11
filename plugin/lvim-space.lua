35f38fcb06a61fb201760dd4405b4b7d7d537e3c
Biser Stoilov <biserstoilov@school-exams.com>
2026-06-11 21:08:37 +0300

    Remove the lazy.nvim plugin guard file

--- plugin/lvim-space.lua
+++
@@ -1,7 +0,0 @@
--- plugin/lvim-space.lua
--- Auto-loaded by Neovim when the plugin directory is on the runtimepath.
--- Prevents double-loading; actual initialisation is done via M.setup() in init.lua.
-if vim.g.loaded_lvim_space then
-    return
-end
-vim.g.loaded_lvim_space = true
